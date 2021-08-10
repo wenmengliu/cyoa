6843989a3e14dee96b09a9bcae776e907396e3f7
Wenmeng Liu <wenmengliu@uvic.ca>
2021-08-10 15:50:04 -0700

    parsing the JSON

---
+++ story.go
@@ -0,0 +1,15 @@
+package cyoa
+
+// Chapter struct is a path in a story
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+type Story map[string]Chapter
